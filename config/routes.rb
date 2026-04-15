a2be771dd870ee494763857000f66b4bd2d739e1
wagurano <wagurano@gmail.com>
2026-04-15 18:03:00 +0900

    health check up 사용합니다

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
+  get '/health', to: 'health#index'
   get '/stats/year/:year', to: 'stats#year'
 
-  resources :posts, only: [:index, :show, :create]
-  resources :disasters, only: [:index, :show]
-  resources :stories, only: [:index, :show]
+  resources :posts, only: %i[index show create]
+  resources :disasters, only: %i[index show]
+  resources :stories, only: %i[index show]
 
   namespace :admin do
     resources :stories
@@ -15,5 +16,5 @@ Rails.application.routes.draw do
   end
 
   mount Rswag::Ui::Engine => '/api-docs'
-  mount Rswag::Api::Engine => '/api-docs'  
+  mount Rswag::Api::Engine => '/api-docs'
 end
